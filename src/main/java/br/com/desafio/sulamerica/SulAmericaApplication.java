e99f689288f76b7ac1c18af72f8cb24eb108dbf8
Leonardo Faria dos Santos <leonardofaria00@gmail.com>
2021-05-03 19:44:28 -0300

    First commit

---
+++ src/main/java/br/com/desafio/sulamerica/SulAmericaApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.sulamerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulAmericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulAmericaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/sulamerica/SulAmericaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.sulamerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulAmericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
