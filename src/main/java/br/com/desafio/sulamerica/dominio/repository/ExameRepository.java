bc93ffdcca11718a3952babfb99da7b829c75aa2
Leonardo Faria dos Santos <leonardofaria00@gmail.com>
2021-05-03 21:22:51 -0300

    Add Repository

--- src/main/java/br/com/desafio/sulamerica/dominio/repository/ExameRepository.java
+++ src/main/java/br/com/desafio/sulamerica/dominio/repository/ExameRepository.java
@@ -1,5 +1,11 @@
 package br.com.desafio.sulamerica.dominio.repository;
 
-public interface ExameRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.desafio.sulamerica.dominio.model.Exame;
+
+@Repository
+public interface ExameRepository extends JpaRepository<Exame, Long> {
 
 }
